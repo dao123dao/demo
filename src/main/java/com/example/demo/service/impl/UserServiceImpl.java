c7e8d4848e35fe66d28effb8cddb888a7d633049
wuyidao <wuyidao@cangoonline.com>
2019-02-13 15:43:06 +0800

    1

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -33,8 +33,8 @@ public class UserServiceImpl implements UserService {
         userMapper.delete();
 
         User u = new User();
-        u.setUsername("lucy");
-        u.setSex("0");
+        u.setUsername("lucy2");
+        u.setSex("1");
         userMapper.insert(u);
 
         User u2 = new User();
